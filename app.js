4afef2716deb288111faf5736dde8267172d0457
HALARYK <greglebarbier@gmail.com>
2026-09-16 20:29:00 +0200

    Load V5.2 campaign routing and real map integration

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
 import { CONFIG } from "./config.js";
 globalThis.HALARYK_CONFIG = CONFIG;
 await import("./shell-v52.js");
+await import("./event-routes.js");
+await import("./v5.2-map.js");
 await import("./app-core.js");
